a8013ddcbcc31ac5987293f857be6f761490df47
ITSimaoAbel <simao@gmail.com>
2024-12-19 14:35:42 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+export const App = () => {
+ 
+
+  return (
+    <>
+    <h1>Hello world! Simao</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum magnam hic adipisci consequatur nemo ipsam minima quisquam repellendus. Sequi velit quidem nesciunt numquam iste ex, expedita dolores quod ad nam!</p>
+    <h2>Hello 2</h2>
+    </>
+  )
+}
+
